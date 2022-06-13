9178d70795685beeca75569cdd3c62502f289bd0
francisxaviere <francisxavi00@gmail.com>
2022-06-13 23:00:38 +0000

    Main header file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * reset_to_98 - reset the value to 98
+ * @n: a pointer point to an integer
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void print_rev(char *s);
+void print_rev(char *s);
+void print_rev(char *s);
+void print_rev(char *s);
+void print_rev(char *s);
+int _atoi(char *s);
+
+#endif
