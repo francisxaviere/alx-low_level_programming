e26c9fc3b9e0a4a4a1c8e393cae38d41e79110e4
francisxaviere <francisxavi00@gmail.com>
2022-06-28 00:28:52 +0000

    New arguments in main function

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - main function
+* @argc: count arg
+* @argv: arg vector
+* Return: zero
+*/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
